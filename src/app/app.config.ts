2b0fc031a304f6da36e7cbb7b18ca011ac12d266
Alexander Bartley Nees <alexbartleynees@gmail.com>
2024-01-20 12:08:09 +1300

    Update to angular 17.1 and use zoneless

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,16 +1,20 @@
-import { ApplicationConfig } from '@angular/core';
+import {
+  ApplicationConfig,
+  ɵprovideZonelessChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
 import { APP_CONFIG } from '@product-feedback-app-v2/app-config';
 import { provideHttpClient } from '@angular/common/http';
-import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { environment } from './environments/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(appRoutes),
     provideHttpClient(),
-    provideAnimations(),
+    provideAnimationsAsync(),
+    ɵprovideZonelessChangeDetection(),
     { provide: APP_CONFIG, useValue: environment },
   ],
 };
