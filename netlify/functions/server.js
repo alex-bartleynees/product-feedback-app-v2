685fb5ed2b29ac8db8b376e2178c98674896b65d
alexbn <alexbartleynees@gmail.com>
2024-03-09 11:59:43 +1300

    Add test

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -1,7 +1,13 @@
 const { app } = require('./server.mjs');
+const fs = require('fs');
 
 exports.handler = (event, context, callback) => {
   console.log('app', app);
+  fs.readdir('/', (err, files) => {
+    files.forEach((file) => {
+      console.log(file);
+    });
+  });
   const server = app();
 
   server(event, context, callback);
