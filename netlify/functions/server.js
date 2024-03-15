d7c4c98f79d8a7daf618823784ae6252c7c797ec
alexbn <alexbartleynees@gmail.com>
2024-03-16 10:15:27 +1300

    Update import

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -1,7 +1,7 @@
 import fs from 'fs';
 
 export default async (request, context) => {
-  const app = require('./server/server.mjs');
+  const app = await import('./server/server.mjs').then((m) => m.app);
   console.log('app', app);
   fs.readdir('/', (err, files) => {
     files.forEach((file) => {
