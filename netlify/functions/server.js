374bd9b04f3471e4c6468d1486f68b0cbe40dda9
alexbn <alexbartleynees@gmail.com>
2024-03-16 10:36:16 +1300

    Debug

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -1,14 +1,23 @@
-const app = require('../dist/product-feedback-app-v2/server/server.mjs');
+const fs = require('fs');
 
 export default async (request, context) => {
-  console.log('app', app);
   console.log(request);
   console.log(context);
-  // fs.readdir('/', (err, files) => {
-  //   files.forEach((file) => {
-  //     console.log(file);
-  //   });
-  // });
+  console.log('reading files');
+  fs.readdir('/', (err, files) => {
+    files.forEach((file) => {
+      console.log(file);
+    });
+  });
+  console.log('done reading files');
+
+  console.log('reading root directory');
+  fs.readdir('../../', (err, files) => {
+    files.forEach((file) => {
+      console.log(file);
+    });
+  });
+  console.log('done reading root directory');
 
   // const server = app();
   // return await server(request, context);
