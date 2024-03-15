def0424a393200937eb7d16ea86ee7068e05c235
alexbn <alexbartleynees@gmail.com>
2024-03-16 11:13:52 +1300

    Debug

--- functions/server.mjs
+++ functions/server.mjs
@@ -1,10 +1,6 @@
 import { app } from '../dist/product-feedback-app-v2/server/server.mjs';
 
 export default async (request, context) => {
-  console.log(request);
-  console.log(context);
-  console.log('app', app);
   const server = app();
-  console.log(server);
-  // return await server(request, context);
+  return await server(request, context);
 };
