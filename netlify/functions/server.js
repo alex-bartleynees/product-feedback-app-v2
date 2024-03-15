d16ec5fb3bea69bf10ed5df4bd352fbbc245765a
alexbn <alexbartleynees@gmail.com>
2024-03-16 10:18:02 +1300

    Update

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -1,7 +1,12 @@
 import fs from 'fs';
+import { posix, sep, relative } from 'path';
+
+const toPosix = (path) => path.split(sep).join(posix.sep);
 
 export default async (request, context) => {
-  const app = await import('./server/server.mjs').then((m) => m.app);
+  const { app } = require(`${toPosix(
+    relative(process.cwd(), __dirname)
+  )}/server/server.mjs`);
   console.log('app', app);
   fs.readdir('/', (err, files) => {
     files.forEach((file) => {
