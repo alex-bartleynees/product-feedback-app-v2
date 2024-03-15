11534f54c63511057176a4cda2924afde8f7faa5
alexbn <alexbartleynees@gmail.com>
2024-03-16 09:56:46 +1300

    Update

--- netlify/functions/server.mjs
+++ netlify/functions/server.mjs
@@ -1,8 +1,5 @@
-import { posix, relative, sep } from 'path';
-import { app } from `${toPosix(relative(process.cwd(), __dirname))}/server/server.mjs`;
+import { app } from './server.mjs';
 import fs from 'fs';
-
-const toPosix = (path) => path.split(sep).join(posix.sep)
 
 export default async (request, context) => {
   console.log('app', app);
