98f2549cf6cf429b8619ff3a48ad566ca29cbf5f
alexbn <alexbartleynees@gmail.com>
2024-03-16 12:12:31 +1300

    Update

--- functions/server.mjs
+++ functions/server.mjs
@@ -4,7 +4,12 @@ import { renderApplication } from '../dist/product-feedback-app-v2/server/render
 export default async (request, context) => {
   const indexHtml = '../dist/product-feedback-app-v2/browser/index.server.html';
   const url = request.url;
-  const document = Buffer.from(indexHtml).toString('base64');
+  const document = Buffer.from(
+    `${JSON.stringify(
+      Buffer.from(html, 'utf-8').toString('base64')
+    )}, 'base64')`.toString('utf-8')
+  );
+
   const html = await renderApplication(bootstrap, {
     url,
     document,
