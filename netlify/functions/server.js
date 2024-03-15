165652e56aad6367f512fa92f4c5a4e641d4aa12
alexbn <alexbartleynees@gmail.com>
2024-03-16 10:08:30 +1300

    Fix

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -1,7 +1,7 @@
-import { app } from './server/server.js';
 import fs from 'fs';
 
 export default async (request, context) => {
+  const app = require('../../dist/product-feedback-app-v2/server/main').app;
   console.log('app', app);
   fs.readdir('/', (err, files) => {
     files.forEach((file) => {
