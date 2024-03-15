c438f7c41b8e08cfb6919d1b6d023da933e6f51b
alexbn <alexbartleynees@gmail.com>
2024-03-16 10:30:09 +1300

    Debug

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -3,12 +3,12 @@ import { app } from '../../dist/product-feedback-app-v2/server/server.mjs';
 
 export default async (request, context) => {
   console.log('app', app);
-  fs.readdir('/', (err, files) => {
-    files.forEach((file) => {
-      console.log(file);
-    });
-  });
+  // fs.readdir('/', (err, files) => {
+  //   files.forEach((file) => {
+  //     console.log(file);
+  //   });
+  // });
 
-  const server = app();
-  return await server(request, context);
+  // const server = app();
+  // return await server(request, context);
 };
