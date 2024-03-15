8024a229496ca5f543eaf10c8e265dff5ab2b48d
alexbn <alexbartleynees@gmail.com>
2024-03-16 11:31:49 +1300

    Update ssr for netlify

--- functions/server.mjs
+++ functions/server.mjs
@@ -1,5 +1,21 @@
-import { app } from '../dist/product-feedback-app-v2/server/server.mjs';
+import { commonEngine } from '../dist/product-feedback-app-v2/server/server.mjs';
+import { bootstrap } from '../dist/product-feedback-app-v2/server/main.server.mjs';
 
 export default async (request, context) => {
-  const server = app();
+  const indexHtml = '../dist/product-feedback-app-v2/browser/index.server.html';
+  const baseUrl = '/';
+  const html = await commonEngine.render({
+    bootstrap,
+    documentFilePath: indexHtml,
+    url: request.url,
+    publicPath: '../dist/product-feedback-app-v2/browser',
+    providers: [{ provide: APP_BASE_HREF, useValue: baseUrl }],
+  });
+
+  return new Response(html, {
+    status: 200,
+    headers: {
+      'Content-Type': 'text/html',
+    },
+  });
 };
