5e48546d81ac32acd531bfb289727eecee185381
alexbn <alexbartleynees@gmail.com>
2024-03-16 13:48:53 +1300

    Fix errors

--- functions/server.mjs
+++ functions/server.mjs
@@ -1,35 +1,11 @@
-import { readFile } from 'node:fs/promises';
-import { join, relative, dirname, resolve } from 'node:path';
+
+import { join, dirname, resolve } from 'node:path';
 import express from 'express';
 import { fileURLToPath } from 'node:url';
-import { readdirSync } from 'node:fs';
 import bootstrap from '../dist/product-feedback-app-v2/server/main.server.mjs';
-import { renderApplication } from '../dist/product-feedback-app-v2/server/render-utils.server.mjs';
 import { CommonEngine } from '@angular/common/http';
 import { APP_BASE_HREF } from '@angular/common';
-import { readdirSync } from 'node:fs';
-import bootstrap from '../dist/product-feedback-app-v2/server/main.server.mjs';
-import { renderApplication } from '../dist/product-feedback-app-v2/server/render-utils.server.mjs';
 
-const getAllFilesIn = (dir) =>
-  readdirSync(dir, { withFileTypes: true }).flatMap((dirent) => {
-    if (dirent.isDirectory()) {
-      return getAllFilesIn(join(dir, dirent.name));
-    }
-    return [join(dir, dirent.name)];
-  });
-
-const document = await readFile(
-  'dist/product-feedback-app-v2/server/index.server.html',
-  'utf-8'
-);
-const staticFiles = getAllFilesIn(
-  join('dist/product-feedback-app-v2/', 'browser')
-).map(
-  (path) =>
-    `/${relative(join('dist/product-feedback-app-v2/', 'browser'), path)}`
-);
-const excludedPaths = [...staticFiles];
 
 export default async (request, context) => {
   const server = express();
