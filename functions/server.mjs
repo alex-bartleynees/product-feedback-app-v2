fca4c97666a876c6843b3a831945e2acd6085877
alexbn <alexbartleynees@gmail.com>
2024-03-16 13:45:39 +1300

    test

--- functions/server.mjs
+++ functions/server.mjs
@@ -1,5 +1,12 @@
 import { readFile } from 'node:fs/promises';
-import { join, relative } from 'node:path';
+import { join, relative, dirname, resolve } from 'node:path';
+import express from 'express';
+import { fileURLToPath } from 'node:url';
+import { readdirSync } from 'node:fs';
+import bootstrap from '../dist/product-feedback-app-v2/server/main.server.mjs';
+import { renderApplication } from '../dist/product-feedback-app-v2/server/render-utils.server.mjs';
+import { CommonEngine } from '@angular/common/http';
+import { APP_BASE_HREF } from '@angular/common';
 import { readdirSync } from 'node:fs';
 import bootstrap from '../dist/product-feedback-app-v2/server/main.server.mjs';
 import { renderApplication } from '../dist/product-feedback-app-v2/server/render-utils.server.mjs';
@@ -25,22 +32,41 @@ const staticFiles = getAllFilesIn(
 const excludedPaths = [...staticFiles];
 
 export default async (request, context) => {
-  console.log('request url', request.url);
-  const url = request.url;
-  if (excludedPaths.includes(url)) {
-    return new Response('Not Found', {
-      status: 404,
-    });
-  }
-  const html = await renderApplication(bootstrap, {
-    url: request.url,
-    document: document.toString(),
-  });
-  console.log('html', html);
-  return new Response(html, {
-    status: 200,
-    headers: {
-      'Content-Type': 'text/html',
-    },
+  const server = express();
+  const serverDistFolder = dirname(
+    fileURLToPath('dist/product-feedback-app-v2/server/')
+  );
+  const browserDistFolder = resolve(serverDistFolder, '../browser');
+  const indexHtml = join(serverDistFolder, 'index.server.html');
+
+  const commonEngine = new CommonEngine();
+
+  server.set('view engine', 'html');
+  server.set('views', browserDistFolder);
+
+  // Example Express Rest API endpoints
+  // server.get('/api/**', (req, res) => { });
+  // Serve static files from /browser
+  server.get(
+    '*.*',
+    express.static(browserDistFolder, {
+      maxAge: '1y',
+    })
+  );
+
+  // All regular routes use the Angular engine
+  server.get('*', (req, res, next) => {
+    const { protocol, originalUrl, baseUrl, headers } = req;
+
+    commonEngine
+      .render({
+        bootstrap,
+        documentFilePath: indexHtml,
+        url: `${protocol}://${headers.host}${originalUrl}`,
+        publicPath: browserDistFolder,
+        providers: [{ provide: APP_BASE_HREF, useValue: baseUrl }],
+      })
+      .then((html) => res.send(html))
+      .catch((err) => next(err));
   });
 };
