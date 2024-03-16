e09ca999a098fa19993185103515286ea86ab3bf
alexbn <alexbartleynees@gmail.com>
2024-03-16 15:00:30 +1300

    Revert changes

--- functions/server.mjs
+++ functions/server.mjs
@@ -1,51 +1,47 @@
-import { join, dirname, resolve } from 'node:path';
-import express from 'express';
-import { fileURLToPath } from 'node:url';
+import { readFile } from 'node:fs/promises';
+import { join, relative } from 'node:path';
+import { readdirSync } from 'node:fs';
 import bootstrap from '../dist/product-feedback-app-v2/server/main.server.mjs';
-import { CommonEngine } from '@angular/ssr';
-import { APP_BASE_HREF } from '@angular/common';
-import '@angular/compiler';
+import { renderApplication } from '../dist/product-feedback-app-v2/server/render-utils.server.mjs';
 
-export default async (request, context) => {
-  const server = express();
-  const serverDistFolder = dirname(
-    fileURLToPath('dist/product-feedback-app-v2/server/')
-  );
-  const browserDistFolder = resolve(serverDistFolder, '../browser');
-  const indexHtml = join(serverDistFolder, 'index.server.html');
-
-  const commonEngine = new CommonEngine();
-
-  server.set('view engine', 'html');
-  server.set('views', browserDistFolder);
-
-  // Example Express Rest API endpoints
-  // server.get('/api/**', (req, res) => { });
-  // Serve static files from /browser
-  server.get(
-    '*.*',
-    express.static(browserDistFolder, {
-      maxAge: '1y',
-    })
-  );
-
-  // All regular routes use the Angular engine
-  server.get('*', (req, res, next) => {
-    const { protocol, originalUrl, baseUrl, headers } = req;
-
-    commonEngine
-      .render({
-        bootstrap,
-        documentFilePath: indexHtml,
-        url: `${protocol}://${headers.host}${originalUrl}`,
-        publicPath: browserDistFolder,
-        providers: [{ provide: APP_BASE_HREF, useValue: baseUrl }],
-      })
-      .then((html) => res.send(html))
-      .catch((err) => next(err));
+const getAllFilesIn = (dir) =>
+  readdirSync(dir, { withFileTypes: true }).flatMap((dirent) => {
+    if (dirent.isDirectory()) {
+      return getAllFilesIn(join(dir, dirent.name));
+    }
+    return [join(dir, dirent.name)];
   });
 
-  server.listen(4000, () => {
-    console.log('Server listening on port 4000');
+const document = await readFile(
+  'dist/product-feedback-app-v2/server/index.server.html',
+  'utf-8'
+);
+const staticFiles = getAllFilesIn(
+  join('dist/product-feedback-app-v2/', 'browser')
+).map(
+  (path) =>
+    `/${relative(join('dist/product-feedback-app-v2/', 'browser'), path)}`
+);
+const excludedPaths = [...staticFiles];
+
+export default async (request, context) => {
+  console.log('request url', request.url);
+  const url = request.url;
+  if (excludedPaths.includes(url)) {
+    return new Response('Not Found', {
+      status: 404,
+    });
+  }
+  const html = await renderApplication(bootstrap, {
+    url: request.url,
+    document,
+    platformProviders: [],
+  });
+  console.log('html', html);
+  return new Response(html, {
+    status: 200,
+    headers: {
+      'Content-Type': 'text/html',
+    },
   });
 };
