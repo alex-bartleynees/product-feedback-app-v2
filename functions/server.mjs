9119d43f88c6a541e600222ff02493c6e5d50700
alexbn <alexbartleynees@gmail.com>
2024-03-17 16:59:41 +1300

    Update function

--- functions/server.mjs
+++ functions/server.mjs
@@ -1,59 +1,17 @@
 import { readFile } from 'node:fs/promises';
-import { join, relative } from 'node:path';
-import { readdirSync } from 'node:fs';
 import bootstrap from '../dist/product-feedback-app-v2/server/main.server.mjs';
 import { renderApplication } from '../dist/product-feedback-app-v2/server/render-utils.server.mjs';
-
-const getAllFilesIn = (dir) =>
-  readdirSync(dir, { withFileTypes: true }).flatMap((dirent) => {
-    if (dirent.isDirectory()) {
-      return getAllFilesIn(join(dir, dirent.name));
-    }
-    return [join(dir, dirent.name)];
-  });
 
 const document = await readFile(
   'dist/product-feedback-app-v2/server/index.server.html',
   'utf-8'
 );
-const staticFiles = getAllFilesIn(
-  join('dist/product-feedback-app-v2/', 'browser')
-).map(
-  (path) =>
-    `/${relative(join('dist/product-feedback-app-v2/', 'browser'), path)}`
-);
-const excludedPaths = [...staticFiles];
 
-export default async (request, context) => {
-  const url = request.url;
-  const fileType = getFileType(url);
-  console.log('fileType', fileType);
-  const staticFileTypes = ['html', 'css', 'js', 'png', 'jpg', 'svg'];
-  console.log(
-    'file type included in static file types',
-    staticFileTypes.includes(fileType)
-  );
-  if (fileType && staticFileTypes.includes(fileType)) {
-    return new Response(await getFile(url), {
-      status: 200,
-      headers: {
-        'Content-Type': getFileType(url),
-      },
-    });
-  }
-
+export default async (request, _context) => {
   let headers = {
-    'Content-Type': 'text/html', // Default content type
-    // 'Cache-Control': 'public, max-age=3600', // Example cache control
+    'Content-Type': 'text/html',
+    'Cache-Control': 'public, max-age=3600',
   };
-
-  if (fileType === 'html') {
-    headers['Content-Type'] = 'text/html';
-  } else if (fileType === 'css') {
-    headers['Content-Type'] = 'text/css';
-  } else if (fileType === 'js') {
-    headers['Content-Type'] = 'application/javascript';
-  }
 
   const html = await renderApplication(bootstrap, {
     url: request.url,
@@ -65,21 +23,3 @@ export default async (request, context) => {
     headers,
   });
 };
-
-function getFileType(url) {
-  console.log('url', url);
-  const parts = url.split('.');
-  return parts[parts.length - 1];
-}
-
-async function getFile(url) {
-  const filePath = splitUrlToGetFile(url);
-  const file = join('dist/product-feedback-app-v2/', 'browser', filePath);
-  console.log('file', file);
-  return await readFile(file, 'utf-8');
-}
-
-function splitUrlToGetFile(url) {
-  const parts = url.split('/');
-  return parts[parts.length - 1];
-}
