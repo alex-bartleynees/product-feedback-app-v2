3fbb7f53dbf79534858d6c829375cad599940e1c
alexbn <alexbartleynees@gmail.com>
2024-05-31 22:48:38 +1200

    Add with fetch

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,14 +5,14 @@ import {
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
 import { APP_CONFIG } from '@product-feedback-app-v2/app-config';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { environment } from './environments/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(appRoutes),
-    provideHttpClient(),
+    provideHttpClient(withFetch()),
     provideAnimationsAsync(),
     provideExperimentalZonelessChangeDetection(),
     { provide: APP_CONFIG, useValue: environment },
