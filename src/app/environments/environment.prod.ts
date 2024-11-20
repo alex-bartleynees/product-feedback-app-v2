4c440121c35b0aaeaa35e804397dd4b7d07ec9f0
Alex Bartley Nees <alexbartleynees@gmail.com>
2024-11-20 22:04:02 +1300

    Update charts and values

--- src/app/environments/environment.prod.ts
+++ src/app/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   apiEndpoint:
     typeof window === 'undefined'
-      ? 'http://svc-product-feedback-api:8080/api/'
-      : 'https://alex-bartleynees.me/product-feedback-app/api/',
+      ? 'http://product-feedback-api-svc:8080/api/'
+      : 'https://alexbartleynees.com/product-feedback-app/api/',
   basePath: '/product-feedback-app',
 };
