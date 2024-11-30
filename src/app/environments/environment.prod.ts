cdde4f8a85e920b8eb9c21bb61b4e69a520618f8
Alex Bartley Nees <alexbartleynees@gmail.com>
2024-11-30 18:00:43 +1300

    Update prod api endpoint

--- src/app/environments/environment.prod.ts
+++ src/app/environments/environment.prod.ts
@@ -1,8 +1,5 @@
 export const environment = {
   production: true,
-  apiEndpoint:
-    typeof window === 'undefined'
-      ? 'http://product-feedback-api-svc:5185/api/'
-      : '/product-feedback-api/api/',
+  apiEndpoint: "https://alexbartleynees.com/product-feedback-api/api/"
   basePath: '/product-feedback-app',
 };
